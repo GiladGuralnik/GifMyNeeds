4621a3b0f0a9ee804033ed189f5a44f77263b75a
jonbir3 <jonbir3@gmail.com>
2019-04-04 02:38:49 +0300

    add child entity annotation

--- app/src/main/java/com/gifmyneeds/database/AppDatabase.java
+++ app/src/main/java/com/gifmyneeds/database/AppDatabase.java
@@ -4,21 +4,24 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import com.gifmyneeds.dao.ChildDao;
 import com.gifmyneeds.dao.UserDao;
+import com.gifmyneeds.models.Child;
 import com.gifmyneeds.models.User;
 
 
-@Database(entities = {User.class}, version = 1, exportSchema = false)
+@Database(entities = {User.class, Child.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase INSTANCE;
 
     public abstract UserDao userDao();
+    public abstract ChildDao childDao();
 
     public static AppDatabase getAppDatabase(Context context) {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                    AppDatabase.class, "gif_my_needs_DB").build();
+                    AppDatabase.class, "gif_my_needs_DB").allowMainThreadQueries().build();
         }
         return INSTANCE;
     }
