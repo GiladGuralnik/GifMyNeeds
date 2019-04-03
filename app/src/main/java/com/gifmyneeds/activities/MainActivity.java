ce6487e8b1ad824980530695362038ac067c0c34
May hagbi <mayhagbi@gmail.com>
2019-04-04 02:50:52 +0300

    add library of json in build.gradle

--- app/src/main/java/com/gifmyneeds/activities/MainActivity.java
+++ app/src/main/java/com/gifmyneeds/activities/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         signinBtn = (Button) findViewById(R.id.signinBtn);
         signupBtn = (Button) findViewById(R.id.signupBtn);
         userName = (EditText) findViewById(R.id.userName);
