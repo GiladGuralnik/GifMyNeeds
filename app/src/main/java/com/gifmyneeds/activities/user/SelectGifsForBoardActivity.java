53b875233acb67109771e9ea254aef7aa961a1ee
Yoel Vaizman <yoelv92@gmail.com>
2019-04-03 22:26:39 +0300

    getting childGifs from SelectCategoryForBoardActivity

--- app/src/main/java/com/gifmyneeds/activities/user/SelectGifsForBoardActivity.java
+++ app/src/main/java/com/gifmyneeds/activities/user/SelectGifsForBoardActivity.java
@@ -1,15 +1,32 @@
 package com.gifmyneeds.activities.user;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.gifmyneeds.R;
+import com.gifmyneeds.models.ChildGifs;
 
 public class SelectGifsForBoardActivity extends AppCompatActivity {
+
+    private  ChildGifs childGifs;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_gifs_for_board_activity_layout);
+
+//        Intent intent = new Intent(SelectCategoryForBoardActivity.this,
+//                SelectGifsForBoardActivity.class);
+//        intent.putExtra("key", "value");
+//        startActivity(intent);
+        Intent incomingIntent = getIntent();
+        //String value = incomingIntent.getStringExtra("key");
+        childGifs = (ChildGifs) incomingIntent.getSerializableExtra("category");
+
+
+
     }
 }
