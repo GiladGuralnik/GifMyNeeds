4c6fbe95ecbf1dfd52e569aa017ef6a35c5d395c
Yoel Vaizman <yoelv92@gmail.com>
2019-04-03 18:44:21 +0300

    Create a class ChildGif

--- app/src/main/java/com/gifmyneeds/models/ChildGifs.java
+++ app/src/main/java/com/gifmyneeds/models/ChildGifs.java
@@ -1,4 +1,12 @@
 package com.gifmyneeds.models;
 
+import java.util.ArrayList;
+
 public class ChildGifs {
+
+    private String childId;
+    private String category;
+    private ArrayList<String> pathGif;
+
+
 }
