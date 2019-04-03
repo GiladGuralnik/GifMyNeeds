6526cf866e662b378c3a9f97000569b3ba8552cb
Yoel Vaizman <yoelv92@gmail.com>
2019-04-03 19:04:45 +0300

    Create add and set

--- app/src/main/java/com/gifmyneeds/models/ChildGifs.java
+++ app/src/main/java/com/gifmyneeds/models/ChildGifs.java
@@ -9,4 +9,27 @@ public class ChildGifs {
     private ArrayList<String> pathGif;
 
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+
+    public ArrayList<String> getPathGif() {
+        return pathGif;
+    }
+
+    public void setPathGif(ArrayList<String> pathGif) {
+        this.pathGif = pathGif;
+    }
+
+
+
+
+
+
 }
