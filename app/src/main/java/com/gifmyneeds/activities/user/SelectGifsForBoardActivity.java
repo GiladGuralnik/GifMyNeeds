9564631cafdcd698b2ed54742e5215b1a736ddb8
Yoel Vaizman <yoelv92@gmail.com>
2019-04-04 04:32:01 +0300

    addit SelectGifsForBoardActivity

--- app/src/main/java/com/gifmyneeds/activities/user/SelectGifsForBoardActivity.java
+++ app/src/main/java/com/gifmyneeds/activities/user/SelectGifsForBoardActivity.java
@@ -1,15 +1,21 @@
 package com.gifmyneeds.activities.user;
 
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 import com.gifmyneeds.R;
+import com.gifmyneeds.activities.MainActivity;
 import com.gifmyneeds.models.ChildGifs;
 
 import java.util.ArrayList;
 
-public class SelectGifsForBoardActivity extends AppCompatActivity {
+public class SelectGifsForBoardActivity extends AppCompatActivity implements View.OnClickListener  {
 
     private  ChildGifs childGifs;
     //Gif from res
@@ -21,21 +27,59 @@ public class SelectGifsForBoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_gifs_for_board_activity_layout);
 
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        Button buttonPlayVideo1 = (Button)findViewById(R.id.button_Video1_1);
+        Button buttonPlayVideo2 = (Button)findViewById(R.id.button_Video1_2);
+
+        VideoView video1 = (VideoView)findViewById(R.id.video_View1_1);
+        String uriPath = "android.resource://com.gifmyneeds/" + R.raw.agree;
+        Uri uri2 = Uri.parse(uriPath);
+        video1.setVideoURI(uri2);
+        video1.requestFocus();
+        video1.start();
+
+
 //        Intent intent = new Intent(SelectCategoryForBoardActivity.this,
 //                SelectGifsForBoardActivity.class);
 //        intent.putExtra("key", "value");
 //        startActivity(intent);
-        Intent incomingIntent = getIntent();
         //String value = incomingIntent.getStringExtra("key");
-        childGifs = (ChildGifs) incomingIntent.getSerializableExtra("category");
+        Intent incomingIntent = getIntent();
+
+        childGifs = (ChildGifs) incomingIntent.getSerializableExtra("child_gif");
         getGifsByCategory(childGifs.getCategory());
-
-
 
     }
 
     private ArrayList<String> getGifsByCategory(String category){
 
         return null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        String uriPath;
+        Uri uri2;
+        switch(view.getId()) {
+            case R.id.button_Video1_1:
+                VideoView v1 = (VideoView)findViewById(R.id.videoView1);
+                uriPath = "android.resource://com.gifmyneeds/" + R.raw.agree;
+                uri2 = Uri.parse(uriPath);
+                v1.setVideoURI(uri2);
+                v1.requestFocus();
+                v1.start();
+                break;
+
+            case R.id.button_Video1_2:
+                VideoView v2 = (VideoView)findViewById(R.id.videoView1);
+                uriPath = "android.resource://com.gifmyneeds/" + R.raw.byebye;
+                uri2 = Uri.parse(uriPath);
+                v2.setVideoURI(uri2);
+                v2.requestFocus();
+                v2.start();
+                break;
+
+        }
+
     }
 }
